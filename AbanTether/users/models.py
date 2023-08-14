4656a7314de353de3be8f5b5750cf36ef1efc2b1
amirrezabsh <amirrezabsh79@gmail.com>
2023-08-15 01:08:58 +0330

    a minor change in a comment :)

--- AbanTether/users/models.py
+++ AbanTether/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00) # Setting the default balance as 100$ for simplicity
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00) 
 
     def __str__(self):
         return self.username
